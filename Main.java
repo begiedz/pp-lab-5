469d2b38a03d2fa7f8cb91805b2e2921c5a88d90
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-05-04 09:08:41 +0200

    zad3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,11 @@ public class Main {
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
 
+            int result = MathUtils.add(10, 20);
+            System.out.println("Result of adding: " + result);
+
             Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("Hello! This is a test email.");
+            messenger.sendMessage("The result of adding 10 and 20 is: " + result);
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
