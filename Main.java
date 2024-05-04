4484b505a6cf2c70f69eed617c2ea838e147e2c0
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-05-04 09:15:28 +0200

    zad4

--- Main.java
+++ Main.java
@@ -6,16 +6,24 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        Person[] people = new Person[5];
+
+        final int b = 10;
+
         try {
-            Person person = new Person("John", 30);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Alice Smith", 25);
+            people[2] = new Person("Bob Johnson", 40);
+            people[3] = new Person("Emily Davis", 35);
+            people[4] = new Person("Michael Brown", 28);
 
-            int result = MathUtils.add(10, 20);
-            System.out.println("Result of adding: " + result);
+            for (Person person : people) {
+                int result = MathUtils.add(person.getAge(), b);
+                System.out.println("Result for " + person.getName() + ": " + result);
 
-            Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("The result of adding 10 and 20 is: " + result);
+                Messenger messenger = new EmailMessenger();
+                messenger.sendMessage("Hello, " + person.getName() + "! Your result is: " + result);
+            }
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
